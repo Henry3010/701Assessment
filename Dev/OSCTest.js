35a682a2dd398336e1156fe7ebd539706fbaa00f
Henry3010 <henry.rehberg@gmx.de>
2023-02-02 18:39:27 +0000

    Fixed Level Control, Added Functions for CP and Preset

--- Dev/OSCTest.js
+++ Dev/OSCTest.js
@@ -5,16 +5,45 @@ const osc = new OSC({
     plugin: new OSC.DatagramPlugin({ send: { port: 8081, host: '192.168.1.177' } })
   });
 
+osc.open();
+let message;
 
-  osc.open();
+function setChannelLevel(channel, intens){
+  message = new OSC.Message(`/eos/user/0/newcmd/Chan ${channel} @ ${intens}#`)
+  osc.send(message)
+}
+function setGroupLevel(group, intens){
+  message = new OSC.Message(`/eos/user/0/newcmd/Group ${group} @ ${intens}#`)
+  osc.send(message)
+}
+function setChannelToCP(channel, CP){
+  message = new OSC.Message(`/eos/user/0/newcmd/Chan ${channel} Color_Palette ${CP}#`)
+  osc.send(message)
+}
+function setGroupToCP(group, CP){
+  message = new OSC.Message(`/eos/user/0/newcmd/Group ${group} Color_Palette ${CP}#`)
+  osc.send(message)
+}
+function setChannelToPreset(channel, preset){
+  message = new OSC.Message(`/eos/newcmd/Chan ${channel} Preset ${preset}#`)
+  osc.send(message)
+}
+function setGroupToPreset(group, preset){
+  message = new OSC.Message(`/eos/newcmd/Group ${group} Preset ${preset}#`)
+  osc.send(message)
+}
 
-  let message = new OSC.Message('/eos/get/patch/count');
-osc.send(message);
-message = new OSC.Message('eos/get/group/count')
-osc.send(message)
+
+
+setChannelLevel(1,75)
+setGroupLevel(1,25)
+setChannelToCP(1,1)
+setGroupToCP(1,1)
+setChannelToPreset(1,1)
+setGroupToPreset(1,2)
 
 const closing = () => {
     osc.close()
 }
 
-setTimeout(closing, 5);
+setTimeout(closing, 1000);
